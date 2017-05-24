aa511fcec7f751ecba6f6e89e4971c66cacaa836
PoweRGbg <roumen_georgiev@yahoo.com>
2017-05-24 07:46:41 +0300

    Added null function smbValue() required for APS interface

--- app/src/main/java/info/nightscout/androidaps/plugins/OpenAPSMA/OpenAPSMAPlugin.java
+++ app/src/main/java/info/nightscout/androidaps/plugins/OpenAPSMA/OpenAPSMAPlugin.java
@@ -119,7 +119,10 @@ public class OpenAPSMAPlugin implements PluginBase, APSInterface {
     public Date getLastAPSRun() {
         return lastAPSRun;
     }
-
+	@Override
+    public Double smbValue() {
+        return null;
+    }
     @Override
     public void invoke(String initiator) {
         log.debug("invoke from " + initiator);
