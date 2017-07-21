0e6509bae824819217590d7cb7e35316467b23bf
PoweRGbg <roumen_georgiev@yahoo.com>
2017-07-21 20:53:11 +0300

    remove smb

--- app/src/main/java/info/nightscout/androidaps/MainApp.java
+++ app/src/main/java/info/nightscout/androidaps/MainApp.java
@@ -33,7 +33,6 @@ import info.nightscout.androidaps.plugins.IobCobCalculator.IobCobCalculatorPlugi
 import info.nightscout.androidaps.plugins.Loop.LoopFragment;
 import info.nightscout.androidaps.plugins.NSClientInternal.NSClientInternalFragment;
 import info.nightscout.androidaps.plugins.OpenAPSAMA.OpenAPSAMAFragment;
-import info.nightscout.androidaps.plugins.OpenAPSSMB.OpenAPSSMBFragment;
 import info.nightscout.androidaps.plugins.OpenAPSMA.OpenAPSMAFragment;
 import info.nightscout.androidaps.plugins.Overview.OverviewFragment;
 import info.nightscout.androidaps.plugins.Persistentnotification.PersistentNotificationPlugin;
@@ -113,7 +112,6 @@ public class MainApp extends Application {
             if (Config.LOOPENABLED) pluginsList.add(LoopFragment.getPlugin());
             if (Config.OPENAPSENABLED) pluginsList.add(OpenAPSMAFragment.getPlugin());
             if (Config.OPENAPSENABLED) pluginsList.add(OpenAPSAMAFragment.getPlugin());
-			if (Config.OPENAPSENABLED) pluginsList.add(OpenAPSSMBFragment.getPlugin());
             pluginsList.add(NSProfileFragment.getPlugin());
             if (Config.OTHERPROFILES) pluginsList.add(SimpleProfileFragment.getPlugin());
             if (Config.OTHERPROFILES) pluginsList.add(LocalProfileFragment.getPlugin());
--- app/src/main/java/info/nightscout/androidaps/plugins/SensitivityWeightedAverage/SensitivityWeightedAveragePlugin.java
+++ app/src/main/java/info/nightscout/androidaps/plugins/SensitivityWeightedAverage/SensitivityWeightedAveragePlugin.java
@@ -26,9 +26,7 @@ import info.nightscout.utils.SafeParse;
  * Created by mike on 24.06.2017.
  */
 
-
 public class SensitivityWeightedAveragePlugin implements PluginBase, SensitivityInterface {
-
     private static Logger log = LoggerFactory.getLogger(SensitivityWeightedAveragePlugin.class);
 
     private static boolean fragmentEnabled = true;
@@ -166,9 +164,7 @@ public class SensitivityWeightedAveragePlugin implements PluginBase, Sensitivity
         double weights = 0;
 
         long hightestWeight = data.keyAt(data.size() - 1);
-
         for (int i = 0; i < data.size(); i++) {
-
             long reversedWeigth = data.keyAt(i);
             double value = data.valueAt(i);
             double weight = (hightestWeight - reversedWeigth) / 2;
@@ -184,8 +180,8 @@ public class SensitivityWeightedAveragePlugin implements PluginBase, Sensitivity
         String sensResult = "";
 
         log.debug("Records: " + index + "   " + pastSensitivity);
-        double average = weightedsum / weights;
 
+        double average = weightedsum / weights;
         double basalOff = average * (60 / 5) / Profile.toMgdl(sens, profile.getUnits());
         double ratio = 1 + (basalOff / profile.getMaxDailyBasal());
 
