e418a30dc921c7fb943ef17304b807b9dcccaeac
AdrianLxM <AdrianLxM@users.noreply.github.com>
2017-10-04 19:42:52 +0200

    RS percent fix

--- app/src/main/java/info/nightscout/androidaps/plugins/PumpDanaRS/DanaRSPlugin.java
+++ app/src/main/java/info/nightscout/androidaps/plugins/PumpDanaRS/DanaRSPlugin.java
@@ -586,7 +586,7 @@ public class DanaRSPlugin implements PluginBase, PumpInterface, DanaRInterface, 
         }
         if (percent > getPumpDescription().maxTempPercent)
             percent = getPumpDescription().maxTempPercent;
-        TemporaryBasal runningTB =  MainApp.getConfigBuilder().getTempBasalFromHistory(System.currentTimeMillis());
+        TemporaryBasal runningTB =  MainApp.getConfigBuilder().getRealTempBasalFromHistory(System.currentTimeMillis());
         if (runningTB != null && runningTB.percentRate == percent) {
             result.enacted = false;
             result.success = true;
