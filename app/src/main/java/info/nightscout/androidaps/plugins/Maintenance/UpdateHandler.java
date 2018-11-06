413c7e6c6f1e7048c36cb3038781c0ce1cd362e2
triplem <160079+triplem@users.noreply.github.com>
2018-11-06 20:59:00 +0100

    idea to read version

---
+++ app/src/main/java/info/nightscout/androidaps/plugins/Maintenance/UpdateHandler.java
@@ -0,0 +1,27 @@
+package info.nightscout.androidaps.plugins.Maintenance;
+
+import org.json.JSONArray;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.net.URL;
+import java.util.List;
+
+import info.nightscout.androidaps.logging.L;
+
+public class UpdateHandler {
+
+    private static final Logger LOG = LoggerFactory.getLogger(L.CORE);
+
+    public List<Version> parse(JSONArray versionsArray) {
+        return null;
+    }
+
+    public JSONArray retrieve(URL url) {
+        return null;
+    }
+
+    
+
+
+}
---
+++ app/src/main/java/info/nightscout/androidaps/plugins/Maintenance/Version.java
@@ -0,0 +1,52 @@
+package info.nightscout.androidaps.plugins.Maintenance;
+
+import java.util.Date;
+
+public class Version {
+
+    private String channel;
+    private Date date;
+    private String version;
+    private String branchTag;
+    private String link;
+
+    public String getChannel() {
+        return channel;
+    }
+
+    public void setChannel(String channel) {
+        this.channel = channel;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    public String getBranchTag() {
+        return branchTag;
+    }
+
+    public void setBranchTag(String branchTag) {
+        this.branchTag = branchTag;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+}
